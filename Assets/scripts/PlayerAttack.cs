c13e8a7dd36418995de7358a8321a9a7604b07a6
Abhishek Raj Singh <mail.avshek@gmail.com>
2025-12-26 22:46:08 +0545

    Added shooting functionality and graphics

--- Assets/scripts/PlayerAttack.cs
+++ Assets/scripts/PlayerAttack.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
     private Animator anim;
     private PlayerMovement PlayerMovement;
     private float cooldownTimer = Mathf.Infinity;
@@ -23,6 +25,21 @@ public class PlayerAttack : MonoBehaviour
         anim.SetTrigger("attack");
         cooldownTimer = 0;
         
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+        //pool fireball
+
+        
+    }
+
+    private int FindFireball() {
+        for (int i = 0; i < fireballs.Length; i++) {
+            if (!fireballs[i].activeInHierarchy) {
+                return i;
+                break;
+            }
+        }
+        return 0;
     }
 
     // private void canAttack() {
